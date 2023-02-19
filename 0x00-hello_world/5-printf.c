247eeb40ee12b0ba663583ea950242b046f9a1b9
Aphiwe <clintaphiwe8@gmail.com>
2023-02-19 07:52:13 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point.
+ * Return: Will print the output using the 'printf' function.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
