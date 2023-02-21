285310a08cb5e1a8e4375631a68d8a70f16146cd
Aphiwe <clintaphiwe8@gmail.com>
2023-02-20 20:18:50 -0800

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * Main - Entry point
+ * main - Entry point
  * Return: 0 (Success)
  */
 
@@ -11,7 +11,7 @@ int main(void)
 {
 	char low;
 
-	for (low = 'z'; low >= 'a'; low++)
+	for (low = 'z'; low >= 'a'; low--)
 	putchar(low);
 	putchar('\n');
 	return (0);
