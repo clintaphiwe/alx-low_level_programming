696e9813c413d8c25324b4652bb7a1d2212c4bb9
Aphiwe <clintaphiwe8@gmail.com>
2023-02-28 07:43:18 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char j;
+
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
