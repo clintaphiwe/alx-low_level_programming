6af42a50b9f3c67636209d9719f27859ea89ef8f
Aphiwe <clintaphiwe8@gmail.com>
2023-02-28 07:15:40 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+_putchar(95); /*95 isthe ascii code for the '_' symbol*/
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
