fdb9a5441d3dca224fc4ae69eae5c3a807663666
Aphiwe <clintaphiwe8@gmail.com>
2023-03-01 03:26:10 -0800

    isDigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
-/**main - Entry point
- * Return: 0 (Success)
+/**
+ * _isdigit -Function that checks if a character is digit
+ * @c: carrier variable
+ * Return: 1 if true, 0 if false
  */
 
-int _sidigit(int c)
+int _isdigit(int c)
 {
 if (c >= '0' && c <= '9')
 return (1);
+
 else
 return (0);
 }
