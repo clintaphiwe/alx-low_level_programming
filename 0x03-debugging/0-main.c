ac96304cae8007bf3e32683944f052dc4aa1b9c5
Aphiwe <clintaphiwe8@gmail.com>
2023-03-01 02:53:18 -0800

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * positive_or_negative() - Function that tests the correct output given.
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+
+a = 0;
+positive_or_negative(a);
+
+return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+
+printf("Infinite loop incoming :(\n");
+
+a = 0;
+/*
+*while (a < 10)
+*{
+*	putchar(a);
+*}
+*/
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
