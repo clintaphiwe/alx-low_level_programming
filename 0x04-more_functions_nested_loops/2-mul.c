7fd9577398195f824d9c6412f5488766d2784f37
Aphiwe <clintaphiwe8@gmail.com>
2023-03-01 03:27:52 -0800

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul -Function that gives the product of a and b
+ * @a: variable
+ * @b: variable
+ *
+ * Return: a * b (Success)
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
