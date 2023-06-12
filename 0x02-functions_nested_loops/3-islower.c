63ac234d14ffba790f236aa9e3af701d5e340f6f
clintaphiwe <clintaphiwe8@gmail.com>
2023-06-11 22:08:41 -0700

    IsLower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,16 +6,10 @@
  * main - Entry point
  * Return: 0 (Success)
  */
-int main(void)
-{
-    int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
+int _islower(int c){
+  if (c >= 'a' && c <= 'z'){
+    return (1);
+  }else
     return (0);
 }
