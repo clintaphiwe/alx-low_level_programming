fb3914147881af12b82e4992061cf082e55bd88a
clintaphiwe <clintaphiwe8@gmail.com>
2023-08-28 18:31:29 +0200

    main.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,15 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
  * simple_print_buffer - prints buffer in hexadecimal
- * @buffer: The address of memory to print
- * @size: The size of memory to print
- * Return: 0
-*/
-
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
 unsigned int i;
+
 i = 0;
 while (i < size)
 {
@@ -28,16 +30,17 @@ printf("\n");
 }
 
 /**
- * main - Function to check the code
- * Return: 0
-*/
-
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 char buffer[98] = {0x00};
+
 simple_print_buffer(buffer, 98);
-_memset(buffer, 0x01, 98);
-printf("-------------------------------\n");
+_memset(buffer, 0x01, 95);
+printf("-------------------------------------------------\n");
 simple_print_buffer(buffer, 98);
 return (0);
 }
