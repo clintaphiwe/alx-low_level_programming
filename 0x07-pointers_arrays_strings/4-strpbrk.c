d8bb3a97ea88c11782d15c944c63557397924b8f
clintaphiwe <clintaphiwe8@gmail.com>
2023-08-28 19:14:51 +0200

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry Point
+ * @s: Input
+ * @accept: Input
+ * Return: 0 (SUCCESS)
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+int k;
+
+while (*s)
+{
+for (k = 0; accept[k]; k++)
+{
+if (*s == accept[k])
+return (s);
+}
+s++;
+}
+return;
+}
