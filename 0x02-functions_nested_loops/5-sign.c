0cc20aadaa9d067963627b2335bec9efef08efd4
clintaphiwe <clintaphiwe8@gmail.com>
2024-02-04 17:56:39 +0200

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,51 +2,26 @@
 #include "main.h"
 
 /**
- * print_sign: Function that prints signs of numbers
+ * print_sign - Function that prints signs of numbers
  * @n: intput
- * RETURNS: 1(POSITIVE)
- * RETURNS: 0 
- * RETURNS: -1(NEGATIVE)
+ * Return: 1 (success)
 */
 
-int print_sign(int n){
-  if (n > 0){
-    _putchar('+');
-    return 1;
-  }
-  else if (n = 0){
-    _putchar('0');
-    return 0;
-  }
-  else{
-    _putchar('-');
-    return -1;
-  }
-}
-
-int main(void)
+int print_sign(int n)
 {
-    int r;
-
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
