49f6bd8cbc37cf40ab0e12242629d65f09e8215b
clintaphiwe <clintaphiwe8@gmail.com>
2024-02-06 18:48:21 +0200

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,22 +9,7 @@
 
 int _abs(int n)
 {
-if (n <= 0)
+if (n >= 0)
+return (n);
 return (-n);
-}
-
-
-int main(void)
-{
-    int r;
-
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    return (0);
 }
