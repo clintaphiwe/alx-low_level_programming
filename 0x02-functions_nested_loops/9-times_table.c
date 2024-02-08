53b9f6e4f7493fad3ae59ae30af592df92b257d3
clintaphiwe <clintaphiwe8@gmail.com>
2024-02-08 16:46:43 +0200

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Function prints the time table.
+ * Return: Always 0
+*/
+
+void times_table(void)
+{
+int c, r;
+for (c = 0; c <= 9; c++)
+{
+for (r = 0; r <= 9; r++)
+{
+printf("%2d,", c * r);
+}
+printf("\n");
+}
+}
