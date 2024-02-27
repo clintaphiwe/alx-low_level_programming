58f380c39ec30044449abdd082dbe0ec05b45dc8
clintaphiwe <clintaphiwe8@gmail.com>
2024-02-27 19:25:10 +0200

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,12 +3,15 @@
  * _pow_recursion - calculates the value of x to the power of y
  * @x: the value
  * @y: the value that x will be raised to
- * return: SUCCESS
- *
+ * Return: (0) SUCCESS
 */
 
 int _pow_recursion(int x, int y)
 {
+if (y < 0)
+{
+return (-1);
+}
 if (y == 0)
 {
 return (1);
