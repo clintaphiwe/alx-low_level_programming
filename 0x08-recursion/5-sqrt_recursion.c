1973547319840ba158db6b0b026e17a3eeb1fc15
clintaphiwe <clintaphiwe8@gmail.com>
2024-08-25 13:33:18 +0200

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,30 @@
-#include <math.h>
-#include <stdio.h>;
+#include "main.h"
 
 /**
- * _sqrt_recursion - Finds the square root of a given number using recursion
- * @n: Number of which we want to find the square root
- * Return: The natural square root of n, or -1 if n does not have a natural square root
+ * _sqrt_helper - A helper function to find the square root.
+ * @n: The number to find the square root of.
+ * @guess: The current guess of the square root.
+ *
+ * Return: The square root of n, or -1 if n has no natural square root.
+ */
+int _sqrt_helper(int n, int guess)
+{
+if (guess * guess == n)
+return (guess);
+if (guess * guess > n)
+return (-1);
+return (_sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to find the square root of.
+ *
+ * Return: The square root of n, or -1 if n has no natural square root.
  */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-{
-return -1;
-}
-if (n <= 1)
-{
-return (n);
-}
-int guess = sqrt(n);
-return (_sqrt_recursion(n / 2));
-}
-
-
-int main(void)
-{
-    int r;
-
-    r = _sqrt_recursion(1);
-    printf("%d\n", r);
-    r = _sqrt_recursion(1024);
-    printf("%d\n", r);
-    r = _sqrt_recursion(16);
-    printf("%d\n", r);
-    r = _sqrt_recursion(17);
-    printf("%d\n", r);
-    r = _sqrt_recursion(25);
-    printf("%d\n", r);
-    r = _sqrt_recursion(-1);
-    printf("%d\n", r);
-    return (0);
+return (-1);
+return (_sqrt_helper(n, 0));
 }
