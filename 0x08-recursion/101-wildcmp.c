71f8c0e14546102d0ed840bbae7dfac47f81953d
clintaphiwe <clintaphiwe8@gmail.com>
2024-08-25 14:21:53 +0200

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,37 +1,28 @@
-#include <stdbool.h>
+#include "main.h"
 
-bool is_match(char *s1, char *s2) {
-    // If both strings are empty, they are identical
-    if (*s1 == '\0' && *s2 == '\0') {
-        return true;
-    }
-
-    // If the first string is empty but the second string is not, they are not identical
-    if (*s1 == '\0') {
-        return false;
-    }
-
-    // If the second string is empty but the first string is not, they are not identical
-    if (*s2 == '\0') {
-        return false;
-    }
-
-    // If the second character is '*', we have two options:
-    // 1. We can ignore the '*' character and move on to the next character in s2.
-    // 2. We can match the '*' character with the current character in s1, and then compare the rest of the string.
-    if (*s2 == '*') {
-        return is_match(s1, s2 + 1) || is_match(s1 + 1, s2);
-    }
-
-    // If the current characters in s1 and s2 are identical, we can move on to the next character in both strings
-    if (*s1 == *s2) {
-        return is_match(s1 + 1, s2 + 1);
-    }
-
-    // If none of the above conditions are met, the strings are not identical
-    return false;
+/**
+ * wildcmp - Compares two strings and checks if they are identical.
+ * @s1: The first string.
+ * @s2: The second string, which can contain the special character *.
+ *
+ * Return: 1 if the strings are identical, 0 otherwise.
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s2 == '\0')
+return (*s1 == '\0');
+if (*s2 == '*')
 }
-
-int wildcmp(char *s1, char *s2) {
-    return is_match(s1, s2) ? 1 : 0;
+if (*(s2 + 1) == '\0') /* If * is the last character in s2 */
+return (1);
+if (wildcmp(s1, s2 + 1)) /* Match * with empty string */
+return (1);
+if (*s1 && wildcmp(s1 + 1, s2)) /* Match * with one or more characters in s1 */
+return (1);
+}
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1)); /* Compare next characters */
+}
+return (0);
 }
